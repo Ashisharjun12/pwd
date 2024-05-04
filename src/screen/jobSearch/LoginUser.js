330d09e057679580e1871bbf0af7a1e4e3d48e2f
YaShRaJ0P <helloiamyashraj0405@gmail.com>
2024-05-04 05:54:44 +0530

    Speech to text in login page

--- src/screen/jobSearch/LoginUser.js
+++ src/screen/jobSearch/LoginUser.js
@@ -1,5 +1,5 @@
-import {View, Text, Modal} from 'react-native';
-import React, { useState } from 'react';
+import { View, Text, Modal } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import {
   responsiveFontSize,
   responsiveHeight,
@@ -9,39 +9,88 @@ import CustomTextInput from '../../common/CustomTextInput';
 import SolidBtn from '../../common/SolidBtn';
 import BorderBtn from '../../common/BorderBtn';
 import { useNavigation } from '@react-navigation/native';
-import firestore from '@react-native-firebase/firestore'
+import firestore from '@react-native-firebase/firestore';
 import Loader from '../../common/Loader';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TouchableOpacity, Image } from 'react-native';
+import Voice from '@react-native-voice/voice';
 
 const LoginUser = () => {
-    const  navigation = useNavigation()
-    
- 
-    
-//email
-    const [email, setEmail] = useState('');
-    const [badEmail, setBadEmail] = useState('');
+  const navigation = useNavigation();
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechResults = onSpeechResults;
 
-    //password
-    const [password, setPassword] = useState('');
-    const [badPaassword, setBadPassword] = useState('');
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
 
-    //loading
-    const [loading ,setLoading] =useState(false)
+  //email
+  const [email, setEmail] = useState('');
+  const [badEmail, setBadEmail] = useState('');
 
+  //password
+  const [password, setPassword] = useState('');
+  const [badPaassword, setBadPassword] = useState('');
 
+  //loading
+  const [loading, setLoading] = useState(false);
 
-      //validation
+  //speech-to-text
+  const [isRecognising, setisRecognising] = useState(false);
+  const [results, setResults] = useState([]);
+
+  const onSpeechStart = e => {
+    console.log(e);
+  };
+  const onSpeechEnd = e => {
+    console.log(e);
+  };
+  const onSpeechResults = e => {
+    setResults(e.value);
+    if (e.value.join("").replace(/\s/g, "").toLowerCase() === ("signin")) {
+      navigation.navigate();
+    }
+    else if (e.value.join("").replace(/\s/g, "").toLowerCase() === ("createaccount")) {
+      navigation.navigate();
+    }
+    if (e.value.join("").replace(/\s/g, "").toLowerCase().includes("emailis")) {
+      const index = e.value.join("").replace(/\s/g, "").indexOf("com");
+      const result = e.value.join("").replace(/\s/g, "").slice(0, index + 3);
+      setEmail(result.substring(7));
+      console.log(email);
+    } else {
+      const result = e.value.join("").replace(/\s/g, "");
+      setPassword(result);
+      console.log(password);
+    }
+  };
+
+  const startRecognising = async () => {
+    try {
+      await Voice.start('en-US');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const stopRecognising = async () => {
+    try {
+      setResults(results);
+      await Voice.stop();
+      await Voice.destroy();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //validation
   const validate = () => {
-  
     let emailRegex = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/;
-
 
     let validEmail = true;
     let validPassword = true;
-
-   
-
 
     //email validate
 
@@ -56,63 +105,60 @@ const LoginUser = () => {
       setBadEmail('');
     }
 
-
-
-
     //password
 
-    if(password==''){
-      validPassword=false;
-      setBadPassword("Please Enter the Password")
-    }else if(password!='' && password.length<6){
-      validPassword=false;
-      setBadPassword('Please Enter min 6 Character')
-    }else if(password!='' && password.length>6){
-      validPassword=true;
-      setBadPassword('')
+    if (password == '') {
+      validPassword = false;
+      setBadPassword('Please Enter the Password');
+    } else if (password != '' && password.length < 6) {
+      validPassword = false;
+      setBadPassword('Please Enter min 6 Character');
+    } else if (password != '' && password.length > 6) {
+      validPassword = true;
+      setBadPassword('');
     }
 
-    return validEmail  && validPassword
+    return validEmail && validPassword;
   };
-
 
   //method handel login
   const handleLogin = () => {
-    setLoading(true)
-    firestore().collection('GetJobs').where('email','==',email).get().then(data=>{
-        setLoading(false)
-        console.log(data.docs)
-        if(data.docs.length>0){
-            data.docs.forEach(item=>{
-                if(item.data().password==password){
-                    setBadEmail("")
-                    setBadPassword("")
-                    goToNextScreen(item.id,item.data().name,item.data().email)
-    
-                }else{
-                    setBadPassword('Wrong Password')
-                }
-            })
-        }else{
-            setBadEmail('No User Exist')
+    setLoading(true);
+    firestore()
+      .collection('GetJobs')
+      .where('email', '==', email)
+      .get()
+      .then(data => {
+        setLoading(false);
+        console.log(data.docs);
+        if (data.docs.length > 0) {
+          data.docs.forEach(item => {
+            if (item.data().password == password) {
+              setBadEmail('');
+              setBadPassword('');
+              goToNextScreen(item.id, item.data().name, item.data().email);
+            } else {
+              setBadPassword('Wrong Password');
+            }
+          });
+        } else {
+          setBadEmail('No User Exist');
         }
-    }).catch(error=>{
-        setLoading(false)
-        console.log(error +'getting login error')
-    })
-  }
-
+      })
+      .catch(error => {
+        setLoading(false);
+        console.log(error + 'getting login error');
+      });
+  };
 
   //nest screen data
-  const goToNextScreen = async(id,email,name)=>{
-    await AsyncStorage.setItem('NAME',name)
-    await AsyncStorage.setItem('EMAIL',email)
-    await AsyncStorage.setItem('USER_ID',id)
-    await AsyncStorage.setItem('USER_TYPE','company')
-    navigation.navigate('DashboardUser')
-
-
-  }
+  const goToNextScreen = async (id, email, name) => {
+    await AsyncStorage.setItem('NAME', name);
+    await AsyncStorage.setItem('EMAIL', email);
+    await AsyncStorage.setItem('USER_ID', id);
+    await AsyncStorage.setItem('USER_TYPE', 'company');
+    navigation.navigate('DashboardUser');
+  };
 
   return (
     <View>
@@ -142,7 +188,7 @@ const LoginUser = () => {
         </Text>
       </View>
       <View>
-      <CustomTextInput
+        <CustomTextInput
           value={email}
           onChangeText={txt => {
             setEmail(txt);
@@ -168,7 +214,7 @@ const LoginUser = () => {
           title={'Password'}
           placeholder={'Password'}
         />
-         {password != '' && (
+        {password != '' && (
           <Text
             style={{
               color: 'red',
@@ -184,23 +230,48 @@ const LoginUser = () => {
             fontSize: responsiveFontSize(2),
             alignSelf: 'flex-end',
             marginRight: responsiveWidth(10),
-            fontWeight:'500'
+            fontWeight: '500',
           }}>
           Forgot Password ?
         </Text>
 
-        <SolidBtn onClick={()=>{
-            if(validate()){
-                handleLogin()
-
+        <SolidBtn
+          onClick={() => {
+            if (validate()) {
+              handleLogin();
             }
-        }} title={'Sign In'}/>
-        <BorderBtn onClick={()=>navigation.navigate('SignUpUser')} title={'Create Account'}/>
+          }}
+          title={'Sign In'}
+        />
+        <BorderBtn
+          onClick={() => navigation.navigate('SignUpUser')}
+          title={'Create Account'}
+        />
       </View>
-      <Loader visible={loading}/>
-
+      <TouchableOpacity
+        onPress={() => {
+          setisRecognising(!isRecognising);
+          isRecognising ? stopRecognising() : startRecognising();
+        }}>
+        {!isRecognising ? (
+          <Image
+            source={require('../../Image/voice.png')}
+            alt="Speech-to-text button"
+            style={{
+              width: responsiveWidth(20.3),
+              height: responsiveHeight(10),
+            }}></Image>
+        ) : (
+          <Image
+            source={require('../../Image/active_voice.png')}
+            style={{
+              width: responsiveWidth(20.3),
+              height: responsiveHeight(10),
+            }}></Image>
+        )}
+      </TouchableOpacity>
+      <Loader visible={loading} />
     </View>
-    
   );
 };
 
--- src/screen/onboarding/Splash.js
+++ src/screen/onboarding/Splash.js
@@ -4,30 +4,30 @@ import { useNavigation } from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Splash = () => {
-    const navigation =useNavigation()
+  const navigation = useNavigation()
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            // getData()
-            navigation.navigate('DashboardUser')
-            // navigation.navigate('SelectUser')
-            // navigation.navigate('BlindAmputee')
-           
-        },1000)
-    },[])
+  useEffect(() => {
+    setTimeout(() => {
+      // getData()
+      // navigation.navigate('DashboardUser')
+      navigation.navigate('SelectUser')
+      // navigation.navigate('BlindAmputee')
 
-    const getData= async()=>{
-      let type = await AsyncStorage.getItem('USER_TYPE')
-      if(type!=null){
-        if(type=='company'){
-          navigation.navigate('DashboardCompany')
-        }
-       
-      }else{
-        navigation.navigate('SelectUser')
+    }, 1000)
+  }, [])
+
+  const getData = async () => {
+    let type = await AsyncStorage.getItem('USER_TYPE')
+    if (type != null) {
+      if (type == 'company') {
+        navigation.navigate('DashboardCompany')
       }
-      
+
+    } else {
+      navigation.navigate('SelectUser')
     }
+
+  }
   return (
     <View>
       <Text>Splash screen</Text>
